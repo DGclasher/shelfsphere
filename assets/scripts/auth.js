b4838cb461db2c976d2c05bef6714ba3671be574
DGclasher <73327721+DGclasher@users.noreply.github.com>
2023-08-09 13:07:21 +0530

    initial

---
+++ assets/scripts/auth.js
@@ -0,0 +1,58 @@
+const baseUrl = "http://127.0.0.1:8000/auth";
+const loginForm = document.getElementById("loginForm");
+if (loginForm) {
+  loginForm.addEventListener("submit", async (e) => {
+    e.preventDefault();
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+    data = { username: username, password: password };
+    try {
+      const response = await fetch(baseUrl + "/login/", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        referrerPolicy: "no-referrer",
+        body: JSON.stringify(data),
+      });
+      localStorage.removeItem("token");
+      localStorage.removeItem("user_id");
+      localStorage.removeItem("group_id");
+      if (response.status == 200) {
+        const responseData = await response.json();
+        localStorage.setItem("token", responseData.access_token);
+        localStorage.setItem("user_id", responseData.user_id);
+        localStorage.setItem('group_id',responseData.group_id)
+        window.location.href = "dashboard.html"
+      } else if (response.status == 403) {
+        console.log(response);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  });
+}
+
+const registerForm = document.getElementById("registerForm");
+if (registerForm) {
+  registerForm.addEventListener("submit", async (e) => {
+    e.preventDefault();
+    const username = document.getElementById("username-register").value;
+    const password = document.getElementById("password-register").value;
+    const roleSelect = document.getElementById("role");
+    const selected = roleSelect.options[roleSelect.selectedIndex].value;
+    try {
+      const data = { username: username, password: password, role: selected };
+      const response = await fetch(baseUrl + "/register/", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        referrerPolicy: "no-referrer",
+        body: JSON.stringify(data),
+      });
+      if (response.status == 200) {
+        console.log("User registered");
+        window.location.href = "login.html"
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  });
+}
---
+++ assets/scripts/dashboard.js
@@ -0,0 +1,199 @@
+const baseUrl = "http://127.0.0.1:8000/api";
+
+document.addEventListener("DOMContentLoaded", () => {
+  if (!localStorage.getItem("token")) {
+    window.location.href = "login.html";
+  }
+});
+if (localStorage.getItem("group_id") == 1) {
+  document.getElementById("memberList").style = "block";
+}
+
+function createListItem(data, targetElement, endpoint) {
+  const listItem = document.createElement("div");
+  listItem.className =
+    "flex items-center justify-between p-4 bg-gray-800 mb-2 rounded";
+
+  const textContainer = document.createElement("div");
+  textContainer.className = "flex-1";
+
+  const title = document.createElement("h2");
+  title.className = "text-xl font-semibold mb-1";
+  title.textContent = data.title || data.user;
+
+  textContainer.appendChild(title);
+  listItem.appendChild(textContainer);
+
+  const actionsContainer = document.createElement("div");
+  actionsContainer.className = "flex space-x-2";
+
+  const editButton = document.createElement("button");
+  editButton.className =
+    "bg-blue-500 px-2 py-1 rounded text-white hover:bg-blue-600";
+  editButton.textContent = "Edit";
+  editButton.setAttribute("data-id", data.id);
+  editButton.setAttribute("endpoint", endpoint);
+  editButton.classList.add("edit-button");
+
+  const deleteButton = document.createElement("button");
+  deleteButton.className =
+    "bg-red-500 px-2 py-1 rounded text-white hover:bg-red-600";
+  deleteButton.textContent = "Delete";
+  deleteButton.classList.add("delete-button");
+  deleteButton.setAttribute("data-id", data.id);
+  deleteButton.setAttribute("endpoint", endpoint);
+
+  actionsContainer.appendChild(editButton);
+  actionsContainer.appendChild(deleteButton);
+  listItem.appendChild(actionsContainer);
+
+  targetElement.appendChild(listItem);
+}
+
+// Function to fetch and display data
+async function fetchDataAndDisplay(endpoint, targetElement) {
+  const token = localStorage.getItem("token");
+  const response = await fetch(baseUrl + endpoint, {
+    method: "GET",
+    headers: { Authorization: `Bearer ${token}` },
+  });
+  if (response.status == 200) {
+    const data = await response.json();
+    data.forEach((item) => {
+      createListItem(item, targetElement, endpoint);
+    });
+  }
+}
+
+// Fetch and display book data and members
+if (localStorage.getItem("group_id") == 1) {
+  fetchDataAndDisplay("/books/", document.getElementById("bookList"));
+  fetchDataAndDisplay("/members/", document.getElementById("memberList"));
+}
+
+async function handleDeleteButtonClick(event) {
+  const targetButton = event.target;
+  const id = targetButton.getAttribute("data-id");
+  const endpoint = targetButton.getAttribute("endpoint") + id + "/";
+  try {
+    const response = await fetch(baseUrl + endpoint, {
+      method: "DELETE",
+      headers: { Authorization: `Bearer ${localStorage.getItem("token")}` },
+    });
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+async function handleEditButtonClick(event) {
+  const targetButton = event.target;
+  const id = targetButton.getAttribute("data-id");
+  const endPoint = targetButton.getAttribute("endpoint");
+  const editUrl = `/pages/edit-${
+    endPoint.split("/")[1]
+  }.html?endpoint=${baseUrl}${endPoint}&id=${id}`;
+  window.location.href = editUrl;
+}
+
+document.addEventListener("click", (event) => {
+  if (event.target.matches(".delete-button")) {
+    handleDeleteButtonClick(event);
+    window.location.replace(window.location.href);
+  }
+});
+document.addEventListener("click", (event) => {
+  if (event.target.matches(".edit-button")) {
+    handleEditButtonClick(event);
+  }
+});
+
+// Functions for member
+function createBookElement(data, targetElement, endpoint) {
+  const bookElement = document.createElement("div");
+  bookElement.classList.add("mb-4");
+
+  const bookTitle = document.createElement("span");
+  bookTitle.textContent = data.title;
+  bookTitle.classList.add("text-white", "text-lg");
+  bookElement.appendChild(bookTitle);
+
+  const bookAuthor = document.createElement("span");
+  bookAuthor.textContent = ` by ${data.author}`;
+  bookAuthor.classList.add("text-gray-400");
+  bookElement.appendChild(bookAuthor);
+
+  const actionButton = document.createElement("button");
+  actionButton.textContent = data.is_available ? "Borrow" : "Return";
+  actionButton.classList.add(
+    "bg-blue-500",
+    "text-white",
+    "px-4",
+    "py-2",
+    "rounded-full",
+    "hover:bg-blue-600",
+    "ml-4"
+  );
+  actionButton.setAttribute("book-id", data.id);
+  actionButton.setAttribute(
+    "endpoint",
+    data.is_available ? "/books/borrow/" : "/books/return/"
+  );
+  actionButton.classList.add("members-action");
+  bookElement.appendChild(actionButton);
+  targetElement.appendChild(bookElement);
+}
+
+async function fetchMemberDataDisplay(endpoint, targetElement) {
+  const token = localStorage.getItem("token");
+  try {
+    const response = await fetch(baseUrl + endpoint, {
+      method: "GET",
+      headers: { Authorization: `Bearer ${token}` },
+    });
+    if (response.status == 200) {
+      const data = await response.json();
+      data.forEach((item) => {
+        createBookElement(item, targetElement, endpoint);
+      });
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+// Fetch view for all books
+if (localStorage.getItem("group_id") == 2) {
+  fetchMemberDataDisplay(
+    "/books/view/all",
+    document.getElementById("allBooks")
+  );
+  fetchMemberDataDisplay(
+    "/books/view/borrowed",
+    document.getElementById("borrowedBooks")
+  );
+}
+
+async function handleBorrowReturn(event) {
+  const targetButton = event.target;
+  const id = parseInt(targetButton.getAttribute("book-id"));
+  const endpoint = targetButton.getAttribute("endpoint");
+  const token = localStorage.getItem("token");
+
+  try {
+    const data = { "book_ids": [id] };
+    const response = await fetch(baseUrl + endpoint, {
+      method: "POST",
+      headers: { Authorization: `Bearer ${token}` },
+      body: JSON.stringify(data),
+    });
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+document.addEventListener("click", (event) => {
+  if (event.target.matches(".members-action")) {
+    handleBorrowReturn(event);
+    window.location.replace(window.location.href);
+  }
+});
